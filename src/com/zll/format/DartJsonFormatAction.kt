66f49c8338ae913c170b0b88953937ae07c40f05
zhanglulin <zhanglulin@meicet.com>
2019-01-15 16:12:17 +0800

    refactor

--- src/com/zll/format/DartJsonFormatAction.kt
+++ src/com/zll/format/DartJsonFormatAction.kt
@@ -1,4 +1,27 @@
 package com.zll.format
 
-class DartJsonFormatAction {
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.util.PsiUtilBase
+import com.zll.format.ui.UiBuilder
+import javax.swing.JFrame
+
+class DartJsonFormatAction : AnAction() {
+    override fun actionPerformed(event: AnActionEvent) {
+        PsiUtilBase.getPsiFileInEditor(event.getData(PlatformDataKeys.EDITOR) as Editor,
+                event.getData(PlatformDataKeys.PROJECT) as Project)
+                ?.let { it.virtualFile }
+                ?.let { UiBuilder(it).build() }
+                ?.let {
+                    JFrame("dart json format").apply {
+                        setSize(700, 470)
+                        defaultCloseOperation = JFrame.DISPOSE_ON_CLOSE
+                        add(it)
+                        isVisible = true
+                    }
+                }
+    }
 }
--- src/com/zll/format/ui/UiBuilder.kt
+++ src/com/zll/format/ui/UiBuilder.kt
@@ -1,4 +1,50 @@
 package com.zll.format.ui
 
-class UiBuilder {
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.components.JBScrollPane
+import com.zll.format.ClassMaker
+import com.zll.format.Util
+import javax.swing.*
+
+class UiBuilder(private val virtualFile: VirtualFile) {
+
+    fun build(): JComponent {
+        return JPanel().apply {
+            placeComponents(this)
+        }
+    }
+
+    private fun placeComponents(panel: JPanel) {
+        panel.layout = null
+
+        val tipLabel = JLabel("input your json and click ok, \ndart class contents will be copied to Clipboard").apply {
+            setBounds(10, 0, 500, 40)
+        }
+
+        val jsonText = JTextArea().apply {
+            setBounds(0,50,700,400)
+        }
+
+        panel.add(JButton("ok").apply {
+            setBounds(600, 10, 80, 30)
+            isVisible = true
+            addActionListener {
+                val classesString = ClassMaker().make(jsonText.text)
+                if (classesString.startsWith("error:")) {
+                    tipLabel.text = classesString
+                } else {
+//                    Util.setSysClipboardText(classesString) // 复制到粘贴板
+                    Util.writeToFile(virtualFile, classesString)
+                }
+            }
+        })
+
+        panel.add(JBScrollPane(jsonText).apply {
+            verticalScrollBarPolicy = JBScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED
+            horizontalScrollBarPolicy = JBScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED
+            setBounds(0, 50, 700, 400)
+        })
+
+        panel.add(tipLabel)
+    }
 }
