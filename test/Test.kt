2a0af04f74bd54baed11772fc4275d915e4a1c07
zhanglulin <413700858@qq.com>
2019-05-16 00:03:34 +0800

    修改测试代码

--- test/Test.kt
+++ test/Test.kt
@@ -8,24 +8,24 @@ import java.io.File
 
 fun main(args: Array<String>) {
     val json = File("test/test.json").readText()
-    val obj = JsonObject().apply {
-//        addProperty("test0", "3")
-//        addProperty("test1", false)
-//        addProperty("test2", 2)
-
-        val array1 = JsonArray()
-        val array2 = JsonArray()
-        val array3 = JsonArray()
-        val array4 = JsonArray()
-        array1.add(array2)
-        array2.add(array3)
-        array2.add(array4)
-        array3.add(1)
-        array3.add(2)
-        array4.add(3)
-        array4.add(4)
-        add("array", array1)
-    }
+//    val obj = JsonObject().apply {
+////        addProperty("test0", "3")
+////        addProperty("test1", false)
+////        addProperty("test2", 2)
+//
+//        val array1 = JsonArray()
+//        val array2 = JsonArray()
+//        val array3 = JsonArray()
+//        val array4 = JsonArray()
+//        array1.add(array2)
+//        array2.add(array3)
+//        array2.add(array4)
+//        array3.add(1)
+//        array3.add(2)
+//        array4.add(3)
+//        array4.add(4)
+//        add("array", array1)
+//    }
 
     JsonParser().parse(json).let {
         if (it is JsonObject)
